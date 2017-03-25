d9c7adbea98a58e714b853fe1b3ac225b6f52420
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-25 16:01:03 -0700

    chat server

--- chat_server/lib/chat_server.ex
+++ chat_server/lib/chat_server.ex
@@ -2,6 +2,30 @@ defmodule ChatServer do
 
   use GenServer
 
+  def start_link do
+    GenServer.start_link(__MODULE__, :ok, name: :chat_room)
+  end
+
+  def get do
+     GenServer.call(:chat_room, {:get})
+  end
+
+  def create(content) do
+    GenServer.cast(:chat_room, {:create, content})
+  end
+
+  def init(:ok) do
+    {:ok, []}
+  end
+
+  def handle_call(request, who, state) do
+
+  end
+
+  def handle_cast(request, state) do
+
+  end
+
   defmodule Message do
     defstruct content: "", username: "anon"
   end
