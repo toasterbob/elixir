8da58a600354f05c87b9cf2b4078b556be3a3ace
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-25 13:07:59 -0700

    fizz buzz

--- practice.ex
+++ practice.ex
@@ -5,12 +5,30 @@ defmodule MyRange do
 end
 
 defmodule FizzBuzz do
-  def fizz_buzz(range) do 
-
+  def fizz_buzz([]) do
+    []
   end
 
   def fizz_buzz(range) do
-
+    [head|tail] = Enum.to_list range
+    [fb_check(head) | fizz_buzz(tail)]
   end
+
+  def fb_check(head) when rem(head, 15) == 0 do
+    "FIZZBUZZ"
+  end
+
+  def fb_check(head) when rem(head, 3) == 0 do
+    "FIZZ"
+  end
+
+  def fb_check(head) when rem(head, 5) == 0 do
+    "BUZZ"
+  end
+
+  def fb_check(head) do
+    head
+  end
+
 
 end
