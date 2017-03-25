0b0bf96175b13736e76dc848393488f4410fc100
Mark Noizumi <mark.noizumi@gmail.com>
2017-03-25 15:27:31 -0700

    chat server

--- chat_server/lib/chat_server.ex
+++ chat_server/lib/chat_server.ex
@@ -12,7 +12,32 @@ defmodule ChatServer do
       :world
 
   """
+  defmodule Message do
+    defstruct content: "", username: "anon"
+  end
+
   def hello do
     :world
   end
+
+  def loop do
+    loop([])
+  end
+
+  def loop(state) do
+    receive do
+      {:get, from} ->
+        send(from, state)
+        loop(state)
+      {:create, message} ->
+        if is_map(message) do
+          structure = struct(Message, message)
+          loop(state ++ [structure])
+        else
+          structure = %Message{content: message}
+          loop(state ++ [structure])
+        end
+    end
+  end
+
 end
